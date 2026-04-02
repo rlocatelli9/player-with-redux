1c729dbc55074d256ca61af0cc6282e5ef8823da
Robson Locatelli <rlocatelli9@gmail.com>
2026-04-02 17:30:20 -0300

    feat: update player slice to handle course modules and improve state management

--- src/features/player/store/slice.spec.ts
+++ src/features/player/store/slice.spec.ts
@@ -6,6 +6,49 @@ describe('Slice Player', () => {
     lessonIndex: 2,
   }
 
+  const modulesMock = [
+    {
+      id: 0,
+      title: "React Fundamentals",
+      lessons: [
+        { id: "NhUr8cwDiiM", title: "What is React?", duration: 600000 },
+        { id: "_gHr2Pe5LCY", title: "Creating the first project", duration: 1200000 }
+      ]
+    },
+    {
+      id: 1,
+      title: "State and Immutability",
+      lessons: [
+        { id: "mY9MLdifqe0", title: "Understanding useState", duration: 900000 },
+        { id: "YMxc9biU9ms", title: "Working with Lists", duration: 750000 }
+      ]
+    },
+    {
+      id: 2,
+      title: "Introduction to Redux",
+      lessons: [
+        { id: "69e1MoUWE1g", title: "Flux and Redux Architecture", duration: 1800000 },
+        { id: "2lxCaLJ2Rbk", title: "Actions and Reducers", duration: 2100000 }
+      ]
+    },
+    {
+      id: 3,
+      title: "Redux Toolkit (RTK)",
+      lessons: [
+        { id: "3aFexvlQ2A8", title: "Simplifying with RTK", duration: 2400000 },
+        { id: "ciTi5tPCKa8", title: "Configuring the Store", duration: 1320000 }
+      ]
+    },
+    {
+      id: 4,
+      title: "Asynchronous Calls",
+      lessons: [
+        { id: "z2XCUu2wIl0", title: "Redux Thunk in Practice", duration: 2700000 },
+        { id: "FnKc64b7bUI", title: "HTTP Requests with Axios", duration: 1980000 }
+      ]
+    }
+  ]
+
   it('should be able to play', () => {
     const initialState = playerSlice.getInitialState()
 
@@ -17,7 +60,12 @@ describe('Slice Player', () => {
   it('should be able to next', () => {
     const initialState = playerSlice.getInitialState()
 
-    const state = playerSlice.reducer(initialState, next())
+    const state = playerSlice.reducer({
+      ...initialState,
+      course: {
+        modules: modulesMock
+      },
+    }, next())
 
     expect(state.current).toEqual({
       moduleIndex: 0,
@@ -31,6 +79,9 @@ describe('Slice Player', () => {
     const state = playerSlice.reducer(
       {
         ...initialState,
+        course: {
+          modules: modulesMock
+        },
         current: {
           moduleIndex: 1,
           lessonIndex: 1,
@@ -48,13 +99,16 @@ describe('Slice Player', () => {
   it('should not be able to jump module or lesson if there is no next lesson available', () => {
     const initialState = playerSlice.getInitialState()
     const current = {
-      moduleIndex: initialState.course.modules.length - 1,
+      moduleIndex: modulesMock.length - 1,
       lessonIndex: 1,
     }
 
     const state = playerSlice.reducer(
       {
         ...initialState,
+        course: {
+          modules: modulesMock
+        },
         current,
       },
       next(),
--- src/features/player/store/slice.ts
+++ src/features/player/store/slice.ts
@@ -4,7 +4,7 @@ import type { RootState } from '../../../core/store'
 
 export interface ModuleProps {
   modules: {
-    id: string
+    id: number
     title: string
     lessons: {
       id: string
@@ -21,7 +21,7 @@ export interface CurrentVideoProps {
 
 export interface VideoState {
   current: CurrentVideoProps
-  course: ModuleProps
+  course: ModuleProps | null
   autoPlay: boolean
 }
 
@@ -30,51 +30,8 @@ const initialState: VideoState = {
     moduleIndex: 0,
     lessonIndex: 0
   },
-  course: {
-    modules: [
-      {
-        id: "0",
-        title: "React Fundamentals",
-        lessons: [
-          { id: "NhUr8cwDiiM", title: "What is React?", duration: 600000 },
-          { id: "_gHr2Pe5LCY", title: "Creating the first project", duration: 1200000 }
-        ]
-      },
-      {
-        id: "1",
-        title: "State and Immutability",
-        lessons: [
-          { id: "mY9MLdifqe0", title: "Understanding useState", duration: 900000 },
-          { id: "YMxc9biU9ms", title: "Working with Lists", duration: 750000 }
-        ]
-      },
-      {
-        id: "2",
-        title: "Introduction to Redux",
-        lessons: [
-          { id: "69e1MoUWE1g", title: "Flux and Redux Architecture", duration: 1800000 },
-          { id: "2lxCaLJ2Rbk", title: "Actions and Reducers", duration: 2100000 }
-        ]
-      },
-      {
-        id: "3",
-        title: "Redux Toolkit (RTK)",
-        lessons: [
-          { id: "3aFexvlQ2A8", title: "Simplifying with RTK", duration: 2400000 },
-          { id: "ciTi5tPCKa8", title: "Configuring the Store", duration: 1320000 }
-        ]
-      },
-      {
-        id: "4",
-        title: "Asynchronous Calls",
-        lessons: [
-          { id: "z2XCUu2wIl0", title: "Redux Thunk in Practice", duration: 2700000 },
-          { id: "FnKc64b7bUI", title: "HTTP Requests with Axios", duration: 1980000 }
-        ]
-      }
-    ]
-  },
-  autoPlay:true,
+  course: null,
+  autoPlay: true,
 }
 
 export const playerSlice = createSlice({
@@ -85,20 +42,22 @@ export const playerSlice = createSlice({
       state.current = action.payload
     },
     next: (state) => {
-      const nextLessonExists = state.current.lessonIndex < state.course.modules[state.current.moduleIndex].lessons.length - 1
-      const nextModuleExists = state.current.moduleIndex < state.course.modules.length - 1
-      if(nextLessonExists){
-        state.current = {
-          ...state.current,
-          lessonIndex: state.current.lessonIndex + 1
+      if (state.course?.modules.length) {
+        const nextLessonExists = state.current.lessonIndex < state.course?.modules[state.current.moduleIndex].lessons.length - 1
+        const nextModuleExists = state.current.moduleIndex < state.course?.modules.length - 1
+        if (nextLessonExists) {
+          state.current = {
+            ...state.current,
+            lessonIndex: state.current.lessonIndex + 1
+          }
+        } else if (nextModuleExists) {
+          state.current = {
+            moduleIndex: state.current.moduleIndex + 1,
+            lessonIndex: 0
+          }
+        } else {
+          console.info('You completed the course')
         }
-      } else if(nextModuleExists){
-        state.current = {
-          moduleIndex: state.current.moduleIndex + 1,
-          lessonIndex: 0
-        }
-      } else {
-        console.info('You completed the course')
       }
     }
   },
@@ -107,10 +66,14 @@ export const playerSlice = createSlice({
 export const { play, next } = playerSlice.actions
 
 export const selectPlayer = (state: RootState) => state.player
-export const selectModule = (state: RootState) => state.player.course.modules
-export const selectCurrentModule = (state: RootState) => state.player.course.modules[state.player.current.moduleIndex]
+export const selectModule = (state: RootState) => state.player.course?.modules || [] as ModuleProps[]
+export const selectCurrentModule = (state: RootState) => {
+  if (!state.player.course?.modules.length) return 0
+  return state.player.course?.modules[state.player.current.moduleIndex]
+}
 export const selectCurrentLesson = (state: RootState) => {
+  if (!state.player.course?.modules.length) return 0
   return state.player.course
-    .modules[state.player.current.moduleIndex]
+    ?.modules[state.player.current.moduleIndex]
     .lessons[state.player.current.lessonIndex]
 }
