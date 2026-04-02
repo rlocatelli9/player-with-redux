d00943ac5b0ff0f31d223d6b41dc0859f20c55b2
Robson Locatelli <rlocatelli9@gmail.com>
2026-04-02 16:21:56 -0300

    refactor: streamline player slice tests for clarity and consistency

--- src/features/player/store/slice.spec.ts
+++ src/features/player/store/slice.spec.ts
@@ -3,18 +3,13 @@ import { next, play, playerSlice } from './slice'
 describe('Slice Player', () => {
   const mockPlay = {
     moduleIndex: 1,
-    lessonIndex: 2
+    lessonIndex: 2,
   }
 
   it('should be able to play', () => {
-
-
     const initialState = playerSlice.getInitialState()
 
-    const state = playerSlice.reducer(
-      initialState,
-      play(mockPlay)
-    )
+    const state = playerSlice.reducer(initialState, play(mockPlay))
 
     expect(state.current).toEqual(mockPlay)
   })
@@ -22,14 +17,49 @@ describe('Slice Player', () => {
   it('should be able to next', () => {
     const initialState = playerSlice.getInitialState()
 
-    const state = playerSlice.reducer(
-      initialState,
-      next()
-    )
+    const state = playerSlice.reducer(initialState, next())
 
     expect(state.current).toEqual({
       moduleIndex: 0,
-      lessonIndex: 1
+      lessonIndex: 1,
     })
+  })
+
+  it('should be able to jump module automatically', () => {
+    const initialState = playerSlice.getInitialState()
+
+    const state = playerSlice.reducer(
+      {
+        ...initialState,
+        current: {
+          moduleIndex: 1,
+          lessonIndex: 1,
+        },
+      },
+      next(),
+    )
+
+    expect(state.current).toEqual({
+      moduleIndex: 2,
+      lessonIndex: 0,
+    })
+  })
+
+  it('should not be able to jump module or lesson if there is no next lesson available', () => {
+    const initialState = playerSlice.getInitialState()
+    const current = {
+      moduleIndex: initialState.course.modules.length - 1,
+      lessonIndex: 1,
+    }
+
+    const state = playerSlice.reducer(
+      {
+        ...initialState,
+        current,
+      },
+      next(),
+    )
+
+    expect(state.current).toEqual(current)
   })
 })
